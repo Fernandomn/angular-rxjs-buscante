070130f2ef5d555e81c3cdf0ff3772b24984625f
Fernando Medeiros do Nascimento <fernando_m_n@hotmail.com>
2024-01-04 17:32:01 -0300

    updating AuthorshipPipe to display correctly all authors

--- src/app/pipes/authorship.pipe.ts
+++ src/app/pipes/authorship.pipe.ts
@@ -6,6 +6,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class AuthorshipPipe implements PipeTransform {
   transform(authors: string[], ...args: unknown[]): string {
     if (authors) {
+      if (args[0] === 'full') {
+        return authors.join(', ');
+      }
       return `${authors[0]}${
         authors.length > 1 ? ', +' + (authors.length - 1) : ''
       }`;
