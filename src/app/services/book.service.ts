de9729b7776650470a09a4a2e5991e2928ed618f
Fernando Medeiros do Nascimento <fernando_m_n@hotmail.com>
2024-01-04 18:25:29 -0300

    finishing implementation of typeahead search, and doing rxjs optimizations

--- src/app/services/book.service.ts
+++ src/app/services/book.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, map, tap } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 import { GoogleBooksSearchResult, Volume } from '../../types/interfaces';
 
 @Injectable({
@@ -11,11 +11,19 @@ export class BookService {
 
   constructor(private http: HttpClient) {}
 
-  search(value: string): Observable<Volume[]> {
-    const params = new HttpParams().append('q', value);
+  search(searchValue: string): Observable<Volume[]> {
+    if (!searchValue) {
+      return of(null);
+    }
 
-    return this.http
-      .get<GoogleBooksSearchResult>(this.apiUrl, { params })
-      .pipe(map((result: GoogleBooksSearchResult) => result.items));
+    const params = new HttpParams().append('q', searchValue);
+
+    return this.http.get<GoogleBooksSearchResult>(this.apiUrl, { params }).pipe(
+      map((result: GoogleBooksSearchResult) => result.items),
+      catchError((error) => {
+        console.error('Erro na busca:', error);
+        return of(null);
+      })
+    );
   }
 }
--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,6 +1,12 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { Subject, Subscription, map, switchMap, takeUntil } from 'rxjs';
+import {
+  Observable,
+  debounceTime,
+  distinctUntilChanged,
+  map,
+  switchMap,
+} from 'rxjs';
 import { LivroVolumeInfo } from 'src/app/models/livro-volume-info';
 import { BookService } from 'src/app/services/book.service';
 import { Volume } from 'src/types/interfaces';
@@ -10,39 +16,20 @@ import { Volume } from 'src/types/interfaces';
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css'],
 })
-export class ListaLivrosComponent implements OnDestroy {
-  listaLivros: LivroVolumeInfo[];
+export class ListaLivrosComponent implements OnInit {
   searchField = new FormControl();
-  subscription: Subscription;
+  foundBooks$: Observable<LivroVolumeInfo[]>;
 
-  onDestroy$ = new Subject<boolean>();
-  foundBooks$ = this.searchField.valueChanges.pipe(
-    takeUntil(this.onDestroy$),
-    switchMap((typedValue: string) => this.bookService.search(typedValue)),
-    map((result: Volume[]) => {
-      console.log('requisiçoes ao servidor');
-      this.listaLivros = this.formatBooks(result);
-    })
-  );
+  constructor(private bookService: BookService) {}
 
-  constructor(private bookService: BookService) {
-    this.onDestroy$.next(false);
+  ngOnInit(): void {
+    this.foundBooks$ = this.searchField.valueChanges.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((typedValue: string) => this.bookService.search(typedValue)),
+      map((result: Volume[]) => (result ? this.formatBooks(result) : null))
+    );
   }
-
-  ngOnDestroy(): void {
-    this.onDestroy$.next(true);
-    this.onDestroy$.unsubscribe();
-  }
-
-  // searchBooks(): void {
-  //   this.subscription = this.bookService.search(this.searchField).subscribe({
-  //     next: (result: Volume[]) => {
-  //       console.log('requisiçoes ao servidor');
-  //       this.listaLivros = this.formatBooks(result);
-  //     },
-  //     error: (error: HttpErrorResponse) => console.error(error),
-  //   });
-  // }
 
   private formatBooks(items: Volume[]): LivroVolumeInfo[] {
     return [...items.map((item) => new LivroVolumeInfo(item))];
